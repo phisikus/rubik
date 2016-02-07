50b823ddb3eaa2022bca6671b5d5fd9bcc6f31ee
phisikus <phisikus@platinum.linux.pl>
2016-02-07 13:18:55 +0100

    Improved POM with jme3 dependencies

---
+++ src/main/scala/pl/linux/platinum/phisikus/EntryPoint.scala
@@ -0,0 +1,12 @@
+package pl.linux.platinum.phisikus
+
+object EntryPoint {
+
+  def foo(x: Array[String]) = x.foldLeft("")((a, b) => a + b)
+
+  def main(args: Array[String]) {
+    println("Hello World!")
+    println("concat arguments = " + foo(args))
+  }
+
+}
